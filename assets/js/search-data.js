4a4c48065928d1cb8f464ff671f024e29a7ebcbb
AnshPrakash <33067038+AnshPrakash@users.noreply.github.com>
2025-09-14 21:51:21 +0000

    Deploying to gh-pages from @ AnshPrakash/anshprakash.github.io@878d5cdaaf43337a0e9f63658fd96af192929870 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -27,7 +27,7 @@ ninja.data = [{
         
           title: "MimicPlay on Franka Arm and its Extension",
         
-        description: "This blog is part of our university’s project lab, where we are working on replicating MimicPlay using a real one-arm robotic platform in our lab. Building on this setup, we aim to extend the approach to bi-manual systems such as the Tiago robot. Our work explores how abundant human play data can be leveraged to guide efficient low-level robot policies.",
+        description: "This blog is part of our university’s project lab, where we are working on replicating MimicPlay using a real one-arm robotic platform in our lab. Building on this setup, we aim to extend the approach to bi-manual systems such as the Tiago robot. Our work explores how abundant human play data can be leveraged to guide efficient low-level robot policies. Original - https://mimic-play.github.io/",
         section: "Posts",
         handler: () => {
           
